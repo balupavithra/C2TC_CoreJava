a8a767929771604816bebc079a84b4dd3f83e274
AGILAN <AGILAN@DESKTOP-DRU42ID>
2024-09-04 14:04:16 +0530

    This is my third commit

---
+++ C2TC_DAY_01/src/Com/tns/day3/Loop.java
@@ -0,0 +1,28 @@
+package Com.tns.day3;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// For loop//
+		for (int i = 0; i < 5; i++) {
+		    System.out.println("i = " + i);
+		}
+         //While loop//
+		int k = 0;
+
+		while (k < 5) {
+		    System.out.println("k = " + k);
+		    k++;
+		}
+        //Do-while//
+		int j = 0;
+
+		do {
+		    System.out.println("j = " + j);
+		    j++;
+		} while (j < 5);
+
+
+	}
+
+}
